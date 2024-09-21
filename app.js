f5c5897a9a79a999f8e04f8c3b12a87dfe45c2f2
alokverma <alokverma749@gmail.com>
2024-09-22 01:35:42 +0530

    fix: version upgrade

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
 import test_route from './routes/v1/testRoute.js';
